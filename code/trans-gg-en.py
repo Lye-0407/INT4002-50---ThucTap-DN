042ba7172d0e18f640dd86a245a7315e76452507
Lye-0407 <0936327550.ce@gmail.com>
2024-07-09 10:24:54 +0700

    forth commit

--- code/trans-gg-en.py
+++ code/trans-gg-en.py
@@ -7,16 +7,12 @@ translated_file_path = 'dataset/transed-gg-en.json'
 with open(file_path, 'r', encoding ='utf-8') as inputfile:
     data = json.load(inputfile)
 
-'''
 def pre_processing(text):
     text = text.replace("\n", " ")
     return " ".join(text.split())
-def gg_translation(data_input):    
-    return GoogleTranslator(source='auto', target='en').translate(pre_processing(data_input))
-'''
 
 def gg_translation(data_input):    
-    return GoogleTranslator(source='auto', target='en').translate((data_input))
+    return GoogleTranslator(source='auto', target='en').translate(pre_processing(data_input))
 
 for query in data:
     relevant_article = query.get('relevant_article', {})
