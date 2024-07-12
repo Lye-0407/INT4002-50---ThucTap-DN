13e32917b5758ebcb8a6cfb4538a0b1f2a1af266
Lye-0407 <0936327550.ce@gmail.com>
2024-07-12 15:24:21 +0700

    4th

--- code/query.py
+++ code/query.py
@@ -1,32 +1,45 @@
 import json
 import os
 
-file_path = './dataset/legal_passages.json'
+file_path1 = './dataset/test.json'
+file_path2 = './dataset/legal_passages.json'
+query_file_path = 'dataset/test_query_file.json'
 
-with open (file_path,'r', encoding='utf-8') as inputfile:
-    inputdata = json.load(inputfile)
+with open (file_path1,'r', encoding='utf-8') as inputfile1:
+    inputdata1 = json.load(inputfile1)
 
-query_id = ""
-query_text = ""
+with open (file_path2,'r', encoding='utf-8') as inputfile2:
+    inputdata2 = json.load(inputfile2)
 
 extracted_data = []
 
-for law in inputdata:
-    law_id = law['id']
-    for article in law['articles']:
-        article_id = article['id']
-        article_text = article['text']
-        extracted_data.append({
-            'query_id': query_id,
-            'query_text': query_text,
-            'relevant_article': {
-                'article_id': article_id,
-                'law_id': law_id,
-                'article_text': article_text
-            }
-        })
+for query1 in inputdata1:
+    example_id = query1['example_id']
+    # label_id = query1['label']
+    statement = query1['statement']
 
-query_file_path = 'dataset/query_file.json'
+    relevant_articles = []
+    for data1 in query1['legal_passages']:
+        law_id = data1['law_id']
+        article_id = data1['article_id']
+        
+        for query2 in inputdata2:
+            passages_id = query2['id']
+            if law_id == passages_id:
+                for data2 in query2['articles']:
+                    articles_id = data2['id']
+                    articles_text = data2['text']
+                    if article_id == articles_id:
+                        map_articles = {"law_id": law_id, "article_id": article_id, "article_text": articles_text}
+        relevant_articles.append(map_articles)
+
+    extracted_data.append({
+                        'query_id': example_id,
+                        # 'label': label_id,
+                        'query_text': statement,
+                        'relevant_article': relevant_articles
+                        })               
+
 with open(query_file_path, 'w', encoding='utf-8') as outputfile:
     json.dump(extracted_data, outputfile, ensure_ascii=False, indent=4)        
 
--- code/trans-gg-en.py
+++ code/trans-gg-en.py
@@ -1,3 +1,5 @@
+import re
+import string
 import json
 from deep_translator import GoogleTranslator
 
@@ -7,34 +9,216 @@ translated_file_path = 'dataset/transed-gg-en.json'
 with open(file_path, 'r', encoding ='utf-8') as inputfile:
     data = json.load(inputfile)
 
-'''
-def pre_processing(text):
-    text = text.replace("\n", " ")
-    return " ".join(text.split())
+# Remove unnecessary characters
+def clean_text_for_segmentation(text: str) -> str:
+    text = re.sub(r'\[\d+\]', ' ', text)
 
-def gg_translation(data_input):    
-    return GoogleTranslator(source='auto', target='en').translate(pre_processing(data_input))
-'''
+    # Remove /n, /t
+    text = re.sub(r'[\n\t]', ' ', text)
+
+    # Remove 1. 2. 3. ... ~1.000.000
+    text = re.sub(r'\b\d+\.\s+', ' ', text)
+
+    # Remove a) b) c) ...
+    text = re.sub(r'[a-zđ]\)', ' ', text)
+
+    # Remove punctuation
+    # text = text.translate(str.maketrans(' ', ' ', string.punctuation))
+    text = re.sub(r'[^\w\s]', ' ', text)
+
+    # Remove multiple spaces
+    text = re.sub(r'\s+', ' ', text)
+
+    # Remove leading and trailing spaces
+    text = text.strip()
+
+    # Lowercase
+    text = text.lower()
+
+    return text
+
+def search(law_id, article_id, query_file):
+    for relevant_article in query_file:
+        if relevant_article["law_id"] == law_id:
+            for article_id in relevant_article:
+                if relevant_article["article_id"] == article_id:
+                    return {
+                        "query_id": query_file["query_id"],
+                        "query_text": query_file["query_text"],
+                        "law_id": law_id,
+                        "article_id": article_id,
+                        "article_text": relevant_article["article_text"],
+                        "article_clause":relevant_article["article_clause"]
+                    }
+    return {
+        "query_id": None,
+        "query_text": None,
+        "law_id": law_id,
+        "article_id": article_id,
+        "article_text": None,
+        "article_clause": None
+    }
     
+def extract_clause(text):
+    # split by \n
+    lines = text.split("\n")
+
+    # remove empty lines
+    lines = [line for line in lines if line != ""]
+
+    # if line[0] start with number
+    if re.match(r"^([0-9]+)\. (.*)$", lines[0]):
+        # Title is None
+        title = None
+
+        clauses = []
+        current_clause = None
+
+        for line in lines:
+            # Use regex to match number
+            match_number = re.match(r"^([0-9]+)\. (.*)$", line)
+            match_alphabet = re.match(r"^([a-z]\)|^\u0111\)) (.*)$", line)
+
+            if match_number:
+                # if current clause is not None
+                if current_clause is not None:
+                    clauses.append(current_clause)
+
+                # create new clause
+                current_clause = {
+                    "clause_id": match_number.group(1),
+                    "clause": match_number.group(2),
+                    "sub_clauses": []
+                }
+            # Use regex to match alphabet
+            elif match_alphabet:
+                # create new sub clause
+                current_clause["sub_clauses"].append(
+                    match_alphabet.group(2)
+                )
+
+            else:
+                # create new sub clause
+                current_clause["sub_clauses"].append(
+                    line
+                )
+
+        # add last clause
+        clauses.append(current_clause)
+
+        return {
+            "title": title,
+            "clauses": clauses
+        }
+
+    # if line[0] does not start with number
+    else:
+        # Title is first line
+        title = lines[0]
+
+        clauses = []
+        current_clause = None
+
+        # first index i that line[i] start with number
+        first_index = 0
+
+        for i in range(len(lines)):
+            if re.match(r"^([0-9]+)\. (.*)$", lines[i]):
+                first_index = i
+                break
+
+        # if first_index == 0: that mean remaining lines does not start with number
+        # so we just return title and remaining lines as sub_clauses of clause_id = 0
+        if first_index == 0:
+            return {
+                "title": title,
+                "clauses": [
+                    {
+                        "clause_id": "0",
+                        "clause": None,
+                        "sub_clauses": lines[1:]
+                    }
+                ]
+            }
+
+        else:
+            # from 1 to first_index - 1 is sub_clauses of clause_id = 0
+            clauses.append({
+                "clause_id": "0",
+                "clause": None,
+                "sub_clauses": lines[1:first_index]
+            })
+
+            # Loop remaining lines
+            for line in lines[first_index:]:
+                # Use regex to match number
+                match_number = re.match(r"^([0-9]+)\. (.*)$", line)
+                match_alphabet = re.match(r"^([a-z]\)|^\u0111\)) (.*)$", line)
+
+                if match_number:
+                    # if current clause is not None
+                    if current_clause is not None:
+                        clauses.append(current_clause)
+
+                    # create new clause
+                    current_clause = {
+                        "clause_id": match_number.group(1),
+                        "clause": match_number.group(2),
+                        "sub_clauses": []
+                    }
+                # Use regex to match alphabet
+                elif match_alphabet:
+                    # create new sub clause
+                    current_clause["sub_clauses"].append(
+                        match_alphabet.group(2)
+                    )
+
+                else:
+                    # create new sub clause
+                    current_clause["sub_clauses"].append(
+                        line
+                    )
+
+            # add last clause
+            clauses.append(current_clause)
+
+            return {
+                "title": title,
+                "clauses": clauses
+            }
+        
 def gg_translation(text):
-
-    translated = GoogleTranslator(source = 'auto', target = 'en').translate(text)
-    return translated
-
+    if text is None:
+        return ""
+    return GoogleTranslator(source = 'auto', target = 'en').translate(text)
 
 for query in data:
     relevant_article = query.get('relevant_article', {})
-
+    print(relevant_article)
+    break
     if 'law_id' in relevant_article:
         relevant_article['law_id'] = gg_translation(relevant_article['law_id'])
-
-
-for query in data:
-    relevant_article = query.get('relevant_article', {})
-
-    if 'article_text' in relevant_article:
-        relevant_article['article_text'] = gg_translation(relevant_article['article_text'])
     
+    for article_text in relevant_article:
+        article_text = extract_clause(article_text)
 
-with open(translated_file_path, 'w', encoding='utf-8') as outputfile:
-    json.dump(data, outputfile, ensure_ascii = False, indent = 4)
+        print("print article after segmentation")
+        print("ARTICLE TEXT", article_text)
+
+        if 'title' in article_text:
+            article_text['title'] = gg_translation(article_text['title'])
+
+        if 'clauses' in article_text:
+            
+            for clause in article_text['clauses']:
+                print(clause)
+                clause = gg_translation(clause['clause'])
+
+            # for sub_clauses in article_text['clauses']: 
+            #     sub_clauses = gg_translation(sub_clauses)
+
+        if 'article_clause' in relevant_article:
+            relevant_article['article_clause'] = gg_translation(relevant_article['article_clause'])    
+
+# with open(translated_file_path, 'w', encoding='utf-8') as outputfile:
+#     json.dump(data, outputfile, ensure_ascii = False, indent = 4)
